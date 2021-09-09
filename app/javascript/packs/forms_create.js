19eec16bccdca40624fe3ebb3f144f3cac63ade1
root <root@JVLA.localdomain>
2021-09-09 12:07:59 +0800

    Fix typo

--- app/javascript/packs/forms_create.js
+++ app/javascript/packs/forms_create.js
@@ -212,7 +212,7 @@ $(document).ready(function(){
                                 <input type="hidden" name="question[id]" value="${result["question_id"]}">
                                 <div id="form_question_${result["question_id"]}_question_div" class="input-group mb-3">
                                     <input type="text" id="form_question_${result["question_id"]}" data-question-id="${result["question_id"]}" name="question[content]" class="form-control form-control-lg question_content_text" placeholder="Question Text">
-                                    <button type="button" type="button" type="button" class="delete_question btn btn-sm btn-outline-danger" data-delete-id="${result["question_id"]}"><i class="far fa-trash-alt"></i> Delete</button>
+                                    <button type="button" class="delete_question btn btn-sm btn-outline-danger" data-delete-id="${result["question_id"]}"><i class="far fa-trash-alt"></i> Delete</button>
                                 </div>
                             </form>
                             <form action="/update_option_content" method="post" class="update_option_content">
@@ -221,7 +221,7 @@ $(document).ready(function(){
                                 <input type="hidden" name="option[id]" value="${result["option_id"]}">
                                 <div id="form_question_${result["question_id"]}_choice_div" class="input-group mb-3 type_multiple_choice">
                                     <input type="text" id="form_question_${result["question_id"]}_choice_${result["option_id"]}" name="option[content]" class="form-control option_content_text" placeholder="Choice Text" data-choice-id=${result["option_id"]}>
-                                    <button type="button" type="button" type="button" class="delete_choice btn btn-sm btn-outline-danger" data-delete-id="${result["option_id"]}"><i class="far fa-trash-alt"></i> Delete</button>
+                                    <button type="button" class="delete_choice btn btn-sm btn-outline-danger" data-delete-id="${result["option_id"]}"><i class="far fa-trash-alt"></i> Delete</button>
                                 </div>
                             </form>
                             <div id="form_question_${result["question_id"]}_add_choice_other_div">
@@ -304,7 +304,7 @@ $(document).ready(function(){
             
             form_question_choice += `
                 <input type="text" id="form_question_${question_number}_choice_${result["option_id"]}" name="option[content]" class="form-control option_content_text" placeholder="Choice Text" data-choice-id="${result["option_id"]}">
-                <button type="button" type="button" class="delete_choice btn btn-sm btn-outline-danger" data-delete-id="${question_number}"><i class="far fa-trash-alt"></i> Delete</button>
+                <button type="button" class="delete_choice btn btn-sm btn-outline-danger" data-delete-id="${question_number}"><i class="far fa-trash-alt"></i> Delete</button>
             `;
             
             if($(`#form_question_${question_number}_div .update_option_content`).last().length > 0){
@@ -337,7 +337,7 @@ $(document).ready(function(){
             <div id="form_question_${$(this).data("add-other-id")}_other_div" class="input-group mb-3">
                 <span class="input-group-text">Other...</span>
                 <input type="text" id="form_question_${question_counter}_choice_${choice_counter}" name="form[form_question_${question_counter}_choice_${choice_counter}]" class="form-control" readonly>
-                <button type="button" type="button" class="delete_other btn btn-sm btn-outline-danger" data-delete-id="${$(this).data("add-other-id")}"><i class="far fa-trash-alt"></i> Delete</button>
+                <button type="button" class="delete_other btn btn-sm btn-outline-danger" data-delete-id="${$(this).data("add-other-id")}"><i class="far fa-trash-alt"></i> Delete</button>
             </div>
         `);
 
